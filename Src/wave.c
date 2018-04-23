ada83b523e1abfe18a48ddac7f110757e74d834e
Noureldin Hendy <nhendy@purdue.edu>
2018-04-23 00:15:01 -0400

    Add files via upload

--- Src/wave.c
+++ Src/wave.c
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include "stm32f0xx_hal.h"
 #include "dac.h"
 #include "tim.h"
@@ -34,6 +35,7 @@ unsigned char buffer2[2];
 struct HEADER header;
 FIL  fil;
 UINT br, bw;
+FRESULT fr;
 
 //used to transmit
 char str[100];
@@ -50,6 +52,9 @@ long bytes_in_each_channel;
 //data buffer containing one sample at a time
 char  data_buffer[100];
 
+uint8_t rbuffer[100];
+
+
 //sample index
 int SampleNumber = 1;
 
@@ -64,7 +69,7 @@ void ParseFile(char * file) {
 // filename = (char*) malloc(sizeof(char) * 1024);
 // if (filename == NULL) {
 //   sprintf(str,"Error in malloc\n");
-//   transmit(str);
+//   //transmit(str);
 //   exit(1);
 // }
 
@@ -84,26 +89,26 @@ void ParseFile(char * file) {
 //	strcat(filename, file);
 	FATFS FatFs;// = malloc(sizeof(FATFS));
 
-	FRESULT fm;
 
 
-	fm = f_mount(&FatFs, USERPath, 1);
 
-	if(fm != FR_OK)
+	fr = f_mount(&FatFs, USERPath, 1);
+
+	if(fr != FR_OK)
 	{
 		sprintf(str, "f_mount fail\n");
-		transmit(str);
+		//transmit(str);
 	}
 
 	sprintf(str, "Opening  file..\n");
-	 transmit(str);
+	 //transmit(str);
 
-	fm = f_open(&fil, file, FA_READ| FA_OPEN_EXISTING);
+	fr = f_open(&fil, file, FA_READ| FA_OPEN_EXISTING);
 
-	if(fm != FR_OK)
+	if(fr != FR_OK)
 	{
 		sprintf(str, "f_open fail\n");
-		transmit(str);
+		//transmit(str);
 		exit(1);
 	}
 
@@ -113,7 +118,7 @@ void ParseFile(char * file) {
 
 
 	sprintf(str,"%s\n", file);
-	transmit(str);
+	//transmit(str);
 
 
 
@@ -124,7 +129,7 @@ void ParseFile(char * file) {
 // ptr = fopen(file, "rb");
 // if (ptr == NULL) {
 //	sprintf(str,"Error opening file\n");
-//	transmit(str);
+//	//transmit(str);
 //	exit(1);
 // }
  
@@ -137,11 +142,11 @@ void ParseFile(char * file) {
 
  f_read(&fil, (unsigned char *) header.riff, sizeof(header.riff),&br);
  sprintf(str,"(1-4): %s \n", header.riff);
- transmit(str);
+ //transmit(str);
 
   read = f_read(&fil,(unsigned char *) buffer4, sizeof(buffer4), &br);
   sprintf(str,"%u %u %u %u\n", buffer4[0], buffer4[1], buffer4[2], buffer4[3]);
-  transmit(str);
+  //transmit(str);
 
 
  // convert little endian to big endian 4 byte int
@@ -151,24 +156,24 @@ void ParseFile(char * file) {
 						(buffer4[3]<<24);
 
  sprintf(str,"(5-8) Overall size: bytes:%u, Kb:%u \n", header.overall_size, header.overall_size/1024);
- transmit(str);
+ //transmit(str);
  read = f_read(&fil, header.wave, sizeof(header.wave), &br);
  sprintf(str,"(9-12) Wave marker: %s\n", header.wave);
- transmit(str);
+ //transmit(str);
  read = f_read(&fil,header.fmt_chunk_marker, sizeof(header.fmt_chunk_marker), &br);
  sprintf(str,"(13-16) Fmt marker: %s\n", header.fmt_chunk_marker);
- transmit(str);
+ //transmit(str);
  read = f_read(&fil,buffer4, sizeof(buffer4), &br);
  sprintf(str,"%u %u %u %u\n", buffer4[0], buffer4[1], buffer4[2], buffer4[3]);
- transmit(str);
+ //transmit(str);
  // convert little endian to big endian 4 byte integer
  header.length_of_fmt = buffer4[0] |
 							(buffer4[1] << 8) |
 							(buffer4[2] << 16) |
 							(buffer4[3] << 24);
  sprintf(str,"(17-20) Length of Fmt header: %u \n", header.length_of_fmt);
- transmit(str);
- read = f_read(&fil, buffer2, sizeof(buffer2), &br); sprintf(str,"%u %u \n", buffer2[0], buffer2[1]);transmit(str);
+ //transmit(str);
+ read = f_read(&fil, buffer2, sizeof(buffer2), &br); sprintf(str,"%u %u \n", buffer2[0], buffer2[1]);//transmit(str);
  
  header.format_type = buffer2[0] | (buffer2[1] << 8);
  char format_name[10] = "";
@@ -180,70 +185,70 @@ void ParseFile(char * file) {
   strcpy(format_name, "Mu-law");
 
  sprintf(str,"(21-22) Format type: %u %s \n", header.format_type, format_name);
- transmit(str);
+ //transmit(str);
  read = f_read(&fil, buffer2, sizeof(buffer2), &br);
  sprintf(str,"%u %u \n", buffer2[0], buffer2[1]);
- transmit(str);
+ //transmit(str);
  header.channels = buffer2[0] | (buffer2[1] << 8);
  sprintf(str,"(23-24) Channels: %u \n", header.channels);
- transmit(str);
+ //transmit(str);
  read = f_read(&fil, buffer4, sizeof(buffer4), &br);
  sprintf(str,"%u %u %u %u\n", buffer4[0], buffer4[1], buffer4[2], buffer4[3]);
- transmit(str);
+ //transmit(str);
  header.sample_rate = buffer4[0] |
 						(buffer4[1] << 8) |
 						(buffer4[2] << 16) |
 						(buffer4[3] << 24);
 
  sprintf(str,"(25-28) Sample rate: %u\n", header.sample_rate);
- transmit(str);
+ //transmit(str);
  read = f_read(&fil, buffer4, sizeof(buffer4), &br);
  sprintf(str,"%u %u %u %u\n", buffer4[0], buffer4[1], buffer4[2], buffer4[3]);
- transmit(str);
+ //transmit(str);
  header.byterate  = buffer4[0] |
 						(buffer4[1] << 8) |
 						(buffer4[2] << 16) |
 						(buffer4[3] << 24);
  sprintf(str,"(29-32) Byte Rate: %u , Bit Rate:%u\n", header.byterate, header.byterate*8);
- transmit(str);
+ //transmit(str);
  read = f_read(&fil, buffer2, sizeof(buffer2), &br);
  sprintf(str,"%u %u \n", buffer2[0], buffer2[1]);
- transmit(str);
+ //transmit(str);
  header.block_align = buffer2[0] |
 					(buffer2[1] << 8);
  sprintf(str,"(33-34) Block Alignment: %u \n", header.block_align);
- transmit(str);
+ //transmit(str);
  read = f_read(&fil, buffer2, sizeof(buffer2), &br);
  sprintf(str,"%u %u \n", buffer2[0], buffer2[1]);
- transmit(str);
+ //transmit(str);
  header.bits_per_sample = buffer2[0] |
 					(buffer2[1] << 8);
 sprintf(str,"(35-36) Bits per sample: %u \n", header.bits_per_sample);
-transmit(str);
+//transmit(str);
  read = f_read(&fil, header.data_chunk_header, sizeof(header.data_chunk_header), &br);
 sprintf(str,"(37-40) Data Marker: %s \n", header.data_chunk_header);
-transmit(str);
+//transmit(str);
  read = f_read(&fil, buffer4, sizeof(buffer4), &br);
 sprintf(str,"%u %u %u %u\n", buffer4[0], buffer4[1], buffer4[2], buffer4[3]);
-transmit(str);
+//transmit(str);
  header.data_size = buffer4[0] |
 				(buffer4[1] << 8) |
 				(buffer4[2] << 16) | 
 				(buffer4[3] << 24 );
 sprintf(str,"(41-44) Size of data chunk: %u \n", header.data_size);
-transmit(str);
+//transmit(str);
 
  // calculate no.of samples
  num_samples = (8 * header.data_size) / (header.channels * header.bits_per_sample);
 sprintf(str,"Number of samples:%lu \n", num_samples);
-transmit(str);
+//transmit(str);
 size_of_each_sample = (header.channels * header.bits_per_sample) / 8;
 sprintf(str,"Size of each sample:%ld bytes\n", size_of_each_sample);
-transmit(str);
+//transmit(str);
  // calculate duration of file
  duration_in_seconds = (float) header.overall_size / header.byterate;
  sprintf(str, "Approx.Duration in seconds=%d\n", duration_in_seconds);
- transmit(str);
+ //transmit(str);
 
 
 
@@ -253,7 +258,7 @@ transmit(str);
  // read each sample from data chunk if PCM
  if (header.format_type == 1) { // PCM
     sprintf(str,"Dump sample data");
-    transmit(str);
+    //transmit(str);
 	char c = 'n';
 //	scanf("%c", &c);
 	if (1)
@@ -265,10 +270,10 @@ transmit(str);
 		// make sure that the bytes-per-sample is completely divisible by num.of channels
 		bytes_in_each_channel = (size_of_each_sample / header.channels);
 		sprintf(str, "bytes in each channel %ld\n", bytes_in_each_channel);
-		transmit(str);
+		//transmit(str);
 		if ((bytes_in_each_channel  * header.channels) != size_of_each_sample) {
 			sprintf(str,"Error: %ld x %ud <> %ld\n", bytes_in_each_channel, header.channels, size_of_each_sample);
-			transmit(str);
+			//transmit(str);
 			size_is_correct = FALSE;
 		}
  
@@ -293,12 +298,12 @@ transmit(str);
 			}					
 
 			sprintf(str,"\n\n.Valid range for data values : %ld to %ld \n", low_limit, high_limit);
-			transmit(str);
+			//transmit(str);
 
 			//LOOP
 //			for (i =1; i <= num_samples; i++) {
 //				sprintf(str,"==========Sample %ld / %ld=============\n", i, num_samples);
-//				transmit(str);
+//				//transmit(str);
 //				read = f_read(&fil, (int * )data_buffer, sizeof(char) , &br);
 //				if (read == FR_OK) {
 //
@@ -308,7 +313,7 @@ transmit(str);
 //
 //					for (xchannels = 0; xchannels < header.channels; xchannels ++ ) {
 //						sprintf(str,"Channel#%d : ", (xchannels+1));
-//						transmit(str);
+//						//transmit(str);
 //						// convert data from little endian to big endian based on bytes in each channel sample
 //						if (bytes_in_each_channel == 4) {
 //							data_in_channel =	data_buffer[0] |
@@ -326,21 +331,21 @@ transmit(str);
 //						}
 //
 //						sprintf(str,"%d ", data_in_channel);
-//						transmit(str);
+//						//transmit(str);
 //						// check if value was in range
 //						if (data_in_channel < low_limit || data_in_channel > high_limit)
 //							sprintf(str,"**value out of range\n");
-//						transmit(str);
+//						//transmit(str);
 //						sprintf(str," | ");
-//						transmit(str);
+//						//transmit(str);
 //					}
 //
 //					sprintf(str,"\n");
-//					transmit(str);
+//					//transmit(str);
 //				}
 //				else {
 //					sprintf(str,"Error reading file. %d bytes\n", read);
-//					transmit(str);
+//					//transmit(str);
 //					break;
 //				}
 //
@@ -348,10 +353,10 @@ transmit(str);
 
 
 
-			for (i =1; i <= num_samples; i++) {
-				readSample(SampleNumber);
-				SampleNumber++;
-			}
+//			for (i =1; i <= num_samples; i++) {
+//				readSample(SampleNumber);
+//				SampleNumber++;
+//			}
 
 		} // 	if (size_is_correct) { 
 		//free(data_buffer);
@@ -361,15 +366,16 @@ transmit(str);
 
 
 // free(data_buffer);
- f_sync(&fil);
- fm = f_close(&fil);
+// f_sync(&fil);
+// fm = f_close(&fil);
+//
+// if(fm == FR_OK)
+// {
+//	 sprintf(str,"Closed file..\n");
+//	 //transmit(str);
+// }
 
- if(fm == FR_OK)
- {
-	 sprintf(str,"Closed file..\n");
-	 transmit(str);
- }
-
+ 	 ReadData();
 
   // cleanup before quitting
  //free(filename);
@@ -397,12 +403,36 @@ extern void HAL_TIM_PeriodElapsedCallback (TIM_HandleTypeDef *
 	if(htim -> Instance == TIM6)
 	{
 		HAL_GPIO_TogglePin(LD3_GPIO_Port, LD3_Pin);
-		readSample(SampleNumber);
-		SampleNumber++;
-		HAL_DAC_SetValue(&hdac1, DAC_CHANNEL_1,DAC_ALIGN_12B_R, data_buffer[0]);
-		HAL_DAC_Start(&hdac1, DAC_CHANNEL_1);
+//		readSample(SampleNumber);
+//		SampleNumber++;
+//		HAL_DAC_SetValue(&hdac1, DAC_CHANNEL_1,DAC_ALIGN_12B_R, data_buffer[0]);
+//		HAL_DAC_Start(&hdac1, DAC_CHANNEL_1);
 
 
+	}
+}
+
+
+void ReadData(void)
+{
+	int stopReading = 0;
+
+	while(!stopReading)
+	{
+		fr = f_read(&fil, rbuffer, sizeof(uint8_t) * 100, &br);
+
+//		int i;
+//		for(i = 0; i < 100; i++)
+//		{
+//			rbuffer[i] = convert(rbuffer[i]);
+//		}
+
+		if(fr != FR_OK)
+		{
+			f_sync(&fil);
+			f_close(&fil);
+			stopReading = 1;
+		}
 	}
 }
 
@@ -415,7 +445,7 @@ int readSample(int counter)
 
 
 	sprintf(str,"==========Sample %ld / %ld=============\n", i, num_samples);
-	transmit(str);
+	//transmit(str);
 	read = f_read(&fil, data_buffer, sizeof(char) * size_of_each_sample, &br);
 	if (read == FR_OK) {
 
@@ -425,7 +455,7 @@ int readSample(int counter)
 
 		for (xchannels = 0; xchannels < header.channels; xchannels ++ ) {
 			sprintf(str,"Channel#%d : ", (xchannels+1));
-			transmit(str);
+			//transmit(str);
 			// convert data from little endian to big endian based on bytes in each channel sample
 			if (bytes_in_each_channel == 4) {
 				data_in_channel =	data_buffer[0] |
@@ -442,21 +472,22 @@ int readSample(int counter)
 			}
 
 			sprintf(str,"%d ", data_in_channel);
-			transmit(str);
+			//transmit(str);
 			// check if value was in range
 			if (data_in_channel < low_limit || data_in_channel > high_limit){
 				sprintf(str,"**value out of range\n");
-			transmit(str);}
+			//transmit(str);
+				}
 			sprintf(str," | ");
-			transmit(str);
+			//transmit(str);
 		}
 
 		sprintf(str,"\n");
-		transmit(str);
+		//transmit(str);
 	}
 	else {
 		sprintf(str,"Error reading file. %d bytes\n", read);
-		transmit(str);
+		//transmit(str);
 		return READ_FAIL;
 
 	}
