be3293689af0cb97de9052fdcb5b3e12573739d3
Nour <nourehab97@gmail.com>
2018-04-25 04:32:29 -0400

    All middleware

--- Middlewares/Third_Party/FatFs/src/diskio.c
+++ Middlewares/Third_Party/FatFs/src/diskio.c
@@ -1,8 +1,5 @@
 /*-----------------------------------------------------------------------*/
 /* Low level disk I/O module skeleton for FatFs     (C)ChaN, 2014        */
-/*                                                                       */
-/*   Portions COPYRIGHT 2015 STMicroelectronics                          */
-/*   Portions Copyright (C) 2014, ChaN, all right reserved               */
 /*-----------------------------------------------------------------------*/
 /* If a working storage control module is available, it should be        */
 /* attached to the FatFs via a glue function rather than modifying it.   */
@@ -10,151 +7,136 @@
 /* storage control modules to the FatFs module with a defined API.       */
 /*-----------------------------------------------------------------------*/
 
-/**
-  ******************************************************************************
-  * @file    diskio.c 
-  * @author  MCD Application Team
-  * @version V1.3.0
-  * @date    08-May-2015
-  * @brief   FatFs low level disk I/O module.
-  ******************************************************************************
-  * @attention
-  *
-  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
-  * You may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at:
-  *
-  *        http://www.st.com/software_license_agreement_liberty_v2
-  *
-  * Unless required by applicable law or agreed to in writing, software 
-  * distributed under the License is distributed on an "AS IS" BASIS, 
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  *
-  ******************************************************************************
-  */
+#include "diskio.h"		/* FatFs lower layer API */
+#include "fat_sd_spi.h"
 
-/* Includes ------------------------------------------------------------------*/
-#include "diskio.h"
-#include "ff_gen_drv.h"
 
-/* Private typedef -----------------------------------------------------------*/
-/* Private define ------------------------------------------------------------*/
-/* Private variables ---------------------------------------------------------*/
-extern Disk_drvTypeDef  disk;
+hwif SD_ff_hw;
 
-/* Private function prototypes -----------------------------------------------*/
-/* Private functions ---------------------------------------------------------*/
 
-/**
-  * @brief  Gets Disk Status 
-  * @param  pdrv: Physical drive number (0..)
-  * @retval DSTATUS: Operation status
-  */
+
+/*-----------------------------------------------------------------------*/
+/* Get Drive Status                                                      */
+/*-----------------------------------------------------------------------*/
+
 DSTATUS disk_status (
 	BYTE pdrv		/* Physical drive nmuber to identify the drive */
 )
 {
-  DSTATUS stat;
-  
-  stat = disk.drv[pdrv]->disk_status(disk.lun[pdrv]);
-  return stat;
+	if (SD_ff_hw.initialized)
+		return 0;
+
+	return STA_NOINIT;
 }
 
-/**
-  * @brief  Initializes a Drive
-  * @param  pdrv: Physical drive number (0..)
-  * @retval DSTATUS: Operation status
-  */
+
+
+/*-----------------------------------------------------------------------*/
+/* Inidialize a Drive                                                    */
+/*-----------------------------------------------------------------------*/
+
 DSTATUS disk_initialize (
 	BYTE pdrv				/* Physical drive nmuber to identify the drive */
 )
 {
-  DSTATUS stat = RES_OK;
-  
-  if(disk.is_initialized[pdrv] == 0)
-  { 
-    disk.is_initialized[pdrv] = 1;
-    stat = disk.drv[pdrv]->disk_initialize(disk.lun[pdrv]);
-  }
-  return stat;
+	if (hwif_init(&SD_ff_hw) == 0)
+		return 0;
+
+	return STA_NOINIT;
 }
 
-/**
-  * @brief  Reads Sector(s) 
-  * @param  pdrv: Physical drive number (0..)
-  * @param  *buff: Data buffer to store read data
-  * @param  sector: Sector address (LBA)
-  * @param  count: Number of sectors to read (1..128)
-  * @retval DRESULT: Operation result
-  */
+
+
+/*-----------------------------------------------------------------------*/
+/* Read Sector(s)                                                        */
+/*-----------------------------------------------------------------------*/
+
 DRESULT disk_read (
 	BYTE pdrv,		/* Physical drive nmuber to identify the drive */
 	BYTE *buff,		/* Data buffer to store read data */
-	DWORD sector,	        /* Sector address in LBA */
+	DWORD sector,	/* Sector address in LBA */
 	UINT count		/* Number of sectors to read */
 )
 {
-  DRESULT res;
- 
-  res = disk.drv[pdrv]->disk_read(disk.lun[pdrv], buff, sector, count);
-  return res;
+	int i;
+
+	for (i=0; i<count; i++)
+		if (sd_read(&SD_ff_hw, sector+i, buff+512*i) != 0)
+			return RES_ERROR;
+
+	return RES_OK;
 }
 
-/**
-  * @brief  Writes Sector(s)  
-  * @param  pdrv: Physical drive number (0..)
-  * @param  *buff: Data to be written
-  * @param  sector: Sector address (LBA)
-  * @param  count: Number of sectors to write (1..128)
-  * @retval DRESULT: Operation result
-  */
-#if _USE_WRITE == 1
+
+
+/*-----------------------------------------------------------------------*/
+/* Write Sector(s)                                                       */
+/*-----------------------------------------------------------------------*/
+
+#if _USE_WRITE
 DRESULT disk_write (
-	BYTE pdrv,		/* Physical drive nmuber to identify the drive */
+	BYTE pdrv,			/* Physical drive nmuber to identify the drive */
 	const BYTE *buff,	/* Data to be written */
 	DWORD sector,		/* Sector address in LBA */
-	UINT count        	/* Number of sectors to write */
+	UINT count			/* Number of sectors to write */
 )
 {
-  DRESULT res;
-  
-  res = disk.drv[pdrv]->disk_write(disk.lun[pdrv], buff, sector, count);
-  return res;
-}
-#endif /* _USE_WRITE == 1 */
+	int i;
 
-/**
-  * @brief  I/O control operation  
-  * @param  pdrv: Physical drive number (0..)
-  * @param  cmd: Control code
-  * @param  *buff: Buffer to send/receive control data
-  * @retval DRESULT: Operation result
-  */
-#if _USE_IOCTL == 1
+	for (i=0; i<count; i++)
+		if (sd_write(&SD_ff_hw, sector+i, buff+512*i) != 0)
+			return RES_ERROR;
+
+	return RES_OK;
+}
+#endif
+
+
+/*-----------------------------------------------------------------------*/
+/* Miscellaneous Functions                                               */
+/*-----------------------------------------------------------------------*/
+
+#if _USE_IOCTL
 DRESULT disk_ioctl (
 	BYTE pdrv,		/* Physical drive nmuber (0..) */
 	BYTE cmd,		/* Control code */
 	void *buff		/* Buffer to send/receive control data */
 )
 {
-  DRESULT res;
-
-  res = disk.drv[pdrv]->disk_ioctl(disk.lun[pdrv], cmd, buff);
-  return res;
+	switch (cmd) {
+	case CTRL_SYNC:
+		return RES_OK;
+	case GET_SECTOR_SIZE:
+		*(WORD*)buff = 512;
+		return RES_OK;
+	case GET_SECTOR_COUNT:
+		*(DWORD*)buff = SD_ff_hw.sectors;
+		return RES_OK;
+	case GET_BLOCK_SIZE:
+		*(DWORD*)buff = SD_ff_hw.erase_sectors;
+		return RES_OK;
+	}
+	return RES_PARERR;
 }
-#endif /* _USE_IOCTL == 1 */
+#endif
 
-/**
-  * @brief  Gets Time from RTC 
-  * @param  None
-  * @retval Time in DWORD
-  */
-__weak DWORD get_fattime (void)
+
+/*
+ * FAT filestamp format:
+ * [31:25] - year - 1980
+ * [24:21] - month 1..12
+ * [20:16] - day 1..31
+ * [15:11] - hour 0..23
+ * [10:5]  - minute 0..59
+ * [4:0]   - second/2 0..29
+ * so... midnight 2009 is 0x3a000000
+ */
+DWORD get_fattime()
 {
-  return 0;
+	int time = 300;
+	int y = 2016, m = 4, d = 4;
+	time %= 86400;
+	return (y-1980)<<25 | m<<21 | d<<16 |
+		(time/3600)<<11 | (time/60%60)<<5 | (time/2%30);
 }
-
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
 
